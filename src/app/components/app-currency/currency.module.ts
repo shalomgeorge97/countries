e6216c71b14269843dcd9a5bd0e16bf6115cf464
Shalom <shalomgeorge97@gmail.com>
2020-03-05 13:50:31 +0530

    test branch

--- src/app/components/app-currency/currency.module.ts
+++ src/app/components/app-currency/currency.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { CurrencyRoutingModule } from './currency-routing.module';
-
+import { CurrencyRoutingModule } from "./currency-routing.module";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    CurrencyRoutingModule
-  ]
+  imports: [CommonModule, CurrencyRoutingModule]
 })
-export class CurrencyModule { }
+export class CurrencyModule {}
+
+// TEST BRANCH
